e3ee75d745132d55902b0a342756ea73e772cf7c
edwinntaadu <108189065+edwinntaadu@users.noreply.github.com>
2024-07-03 22:58:59 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Nav from "./Nav";
+import Logo from "../assets/Logo.svg";
+import FooterContact from "./Footer/FooterContact";
+import SocialMediaLinks from "./Footer/SocialMediaLinks";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container-custom">
+          <div className="footer-container d-md-flex align-items-start justify-content-between mb-5">
+            <div className="d-flex justify-content-center align-items-start">
+              <img src={Logo} alt="logo" />
+            </div>
+            <div>
+              <Nav />
+            </div>
+            <div>
+              <FooterContact />
+            </div>
+            <div>
+              <SocialMediaLinks />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
